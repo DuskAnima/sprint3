051af9c513a5d0376543b337f55dce7eeaca79ec
Anima <cjaccardh@gmail.com>
2023-04-24 22:44:41 -0400

    Sprint 3 finalizado y funcional

--- main.py
+++ main.py
@@ -1,78 +1,40 @@
 import random
 import string
 
-"""
 #función para ingresar teléfono
 def usernumber(u_num): 
     chara = len(u_num) #variable de longitud
     n = u_num.isdigit() #revisa si el input contiene solo numeros
     if n == False: #check de isdigit
         print("Por favor, solo ingrese numeros")
-    if not chara == 8: 
+    elif not chara == 8: 
         print("Por favor, ingrese 8 caracteres")
-    if chara == 8 and n == True:
+    elif chara == 8 and n == True:
         return True #Verdadero si el tamaño es mayor a 5 y menor a 13
-        
 
+#función para contraseñas aleatorias
+def password():
+    return ''.join(random.choice(string.hexdigits) for _ in range(10))
 
-correcto = False
-while correcto == False:
-        numero_t = input("Ingrese su teléfono: ")
-        if usernumber(numero_t) == True:
-            print("Teléfono ingresado exitosamente")
-            correcto=True
-
-"""
-
-
-
+#listas que juntan los datos
 usuarios = ["user1","user2","user3","user4","user5","user6","user7","user8","user9","user10"]
-contraseñas =[]#se guardan las claves en esta variable
+contraseñas =[]
+telefonos = []
 
-cuentas = list(zip(usuarios,contraseñas))
-print(cuentas)
-#def password(passw_l):
-
-done = True
-while done == False:
-    chara = list(string.ascii_letters) + list(string.digits)
-    temp = [random.choice(chara) for i in range(10)]
-    
-    
-    
-    passw = "".join(temp)
-
-
-
-
-"""
-
-for i in range():
-    temp = random.sample(chara)
-    passw = temp
-print(passw)
-
-
-"""
-
-
-"""
-
-
-usuarios = []
-contraseñas = []
-telefono = []
-
-
-correcto = False
-while correcto == False:
-        nombre = input("Ingrese nombre de usuario: ")
-        if username(nombre) == True:
-            print("Usuario creado exitosamente")
-            correcto=True
-
-while correcto == True:
-    print(f"Su clave es: {password}")
+while len(telefonos) < 10:
     correcto = False
+    while correcto == False:
+            numero_t = input("Ingrese su teléfono: ")
+            if usernumber(numero_t) == True:
+                telefonos.append(numero_t)
+                print("Teléfono ingresado exitosamente")
+                correcto=True
 
-"""
+    correcto = True
+    while correcto == True:
+        contraseñas.append(password())
+        print(password())
+        correcto = False
+    cuentas = list(zip(usuarios,contraseñas,telefonos))
+    print(cuentas)
+print("Usuarios registrados exitosamente")
